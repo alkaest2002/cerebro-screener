b5766e1fe5d2a54fbe0e563b3d305b68a7e0a10e
Pierpaolo Calanna <p.calanna@gmail.com>
2021-04-19 14:32:24 +0200

    update task dom demo

--- src/views/tasks/dom/dom-demo.vue
+++ src/views/tasks/dom/dom-demo.vue
@@ -4,9 +4,11 @@
     <template #explanation>
       <div class="mt-6">
         <p class="has-text-grey">
-          Nell'esempio proposto, le tessere in alto sembrano alternarsi.
-          Trascina sulle facce dell'ultima tessera (quella con i punti
-          interrogativi), i numeri che ritieni completino la serie.
+          <b>Come devi procedere</b>: individua il criterio che lega tra loro le
+          tessere in alto e completa la tessera mancante. Trascina e rilascia
+          sulle facce dell'ultima tessera (quella con i punti interrogativi), i
+          numeri che ritieni completino la serie. Se sbagli, puoi trascinare
+          nuovi numeri su quelli che intendi cambiare.
         </p>
       </div>
     </template>
