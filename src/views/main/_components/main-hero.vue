67a5bade35737b9025a90bf945cefc1a1f61776a
Pierpaolo Calanna <p.calanna@gmail.com>
2021-04-20 10:31:21 +0200

    added lang version

--- src/views/main/_components/main-hero.vue
+++ src/views/main/_components/main-hero.vue
@@ -2,7 +2,9 @@
   <div class="has-text-centered">
     <img src="@/assets/images/logo.svg" style="height: 200px" />
     <div class="block">
-      <h1 class="is-size-3 has-text-weight-bold">Cerebro Screener - {{ $store.state.main.version }}</h1>
+      <h1 class="is-size-3 has-text-weight-bold">
+        Cerebro Screener - {{ $store.state.main.version }}
+      </h1>
       <slot>
         <p>Assessment of mental health through cognitive tasks</p>
       </slot>
