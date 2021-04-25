508b7a8ac7f72f7cf0b416e65c9a7c1cbcda50a4
alkaest2002 <p.calanna@gmail.com>
2021-04-25 16:07:52 +0200

    implementing mul task

---
+++ src/views/tasks/mul/_components/mul-figure.vue
@@ -0,0 +1,96 @@
+<template>
+  <div 
+    class="card is-relative is-flex is-align-items-center is-justify-content-center"
+    :class="cardClass"
+  />
+</template>
+
+<script>
+export default {
+  // name
+  name: "mul-figure",
+
+  // props
+  props: {
+    cardClass: {
+      type: String,
+      required: true
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+$dim: 100px;
+
+.card {
+  height: $dim;
+  width: $dim;
+  color: #fff;
+  font-size: $dim*.85;
+  
+  &.circle {
+    border-radius: 50%;
+  }
+  
+  &.rhombus {
+    transform: rotate(45deg);
+    
+    &::after{
+      transform: rotate(-45deg);
+    }
+  }
+  
+  &.a::after {
+    content: "A";
+  }
+  
+  &.b::after {
+    content: "B";
+  }
+  
+   &.c::after {
+    content: "C";
+  }
+  
+   &.d::after {
+    content: "D";
+  }
+  
+   &.e::after {
+    content: "E";
+  }
+  
+   &.f::after {
+    content: "F";
+  }
+  
+  &.g::after {
+    content: "G";
+  }
+  
+  &.h::after {
+    content: "H";
+  }
+    
+  &.j::after {
+    content: "J";
+  }
+  
+  &.k::after {
+    content: "K";
+  }
+  
+  &.red {
+    background-color: red;
+  }
+
+  &.green {
+     background-color: green;
+  }
+
+  &.blue {
+     background-color: blue;
+  }
+}
+</style>
