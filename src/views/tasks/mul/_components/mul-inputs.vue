6ec0533a859707d8c4e2de08b66499bf6dbbc4cc
Pierpaolo Calanna <p.calanna@gmail.com>
2021-05-03 19:49:54 +0200

    code cleanup

--- src/views/tasks/mul/_components/mul-inputs.vue
+++ src/views/tasks/mul/_components/mul-inputs.vue
@@ -46,16 +46,15 @@ export default {
   // name
   name: "mul-inputs",
 
-  // do not inherit attrs
-  inheritAttrs: false,
-  
   // components
   components: {
     mulInput,
     mulKeyboard,
   },
 
-
+  // do not inherit attrs
+  inheritAttrs: false,
+  
   // emitted events
   emits: {
     "user-response": (value) => Array.isArray(value),
