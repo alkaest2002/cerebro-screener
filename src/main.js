7a0260d8b2f725a49789f7a4a8a7cd5e8919e5b1
alkaest2002 <p.calanna@gmail.com>
2021-08-29 21:40:15 +0200

    simplified code

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 import { createApp } from "vue";
 import { store } from "@/store";
-
 import app from "./app.vue";
 import router from "@/router";
-
-// import global components
 import baseLayout from "@/views/_components/base-layout";
 import loadingButton from "@/views/_components/loading-button";
 
-// add bulma
+// bulma
 require("./assets/sass/bulma.scss");
 
-// create app
+// create app with vuex store and router
 const vueApp = createApp(app).use(store).use(router);
 
 // add global components
